7b1026312524469cfb5490ce5b81c1499d7dd6e1
Khurram Shahzad <kannethchamp@gmail.com>
2025-02-08 02:47:57 +0500

    UI INPUT

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,5 @@
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { cn } from "../../lib/utils";  // Adjusted the path based on typical Next.js folder structure
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
@@ -14,9 +13,10 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
 
-export { Input }
+Input.displayName = "Input";
+
+export { Input };
